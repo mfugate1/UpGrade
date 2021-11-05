99bc548657cf6237c9038a993632978b6b02faad
pratik <ppratik.cr7@gmail.com>
2021-11-05 19:25:20 +0530

    improving code for consistent init call to return values from upsert query

--- backend/packages/Upgrade/src/api/repositories/ExperimentUserRepository.ts
+++ backend/packages/Upgrade/src/api/repositories/ExperimentUserRepository.ts
@@ -6,6 +6,35 @@ type UserInput = Omit<ExperimentUser, 'createdAt' | 'updatedAt' | 'versionNumber
 
 @EntityRepository(ExperimentUser)
 export class ExperimentUserRepository extends Repository<ExperimentUser> {
+  public async upsertExperimentUser(user: Partial<ExperimentUser>): Promise<ExperimentUser> {
+    var updateStatement = `("id") DO UPDATE SET "group" = :group, "workingGroup" = :workingGroup`;
+    if (!user.group) { 
+      var updateStatement = `("id") DO UPDATE SET "workingGroup" = :workingGroup`;
+    }
+    if (!user.workingGroup) { 
+      var updateStatement = `("id") DO UPDATE SET "group" = :group`;
+    }
+    if (!user.workingGroup && !user.group) { 
+      var updateStatement = `("id") DO UPDATE SET "id" = :id`;
+    }
+
+    const result = await this.createQueryBuilder()
+      .insert()
+      .into(ExperimentUser)
+      .values(user)
+      .onConflict(updateStatement)
+      .setParameter('id', user.id)
+      .setParameter('group', user.group)
+      .setParameter('workingGroup',user.workingGroup)
+      .returning('*')
+      .execute()
+      .catch((errorMsg: any) => {
+        const errorMsgString = repositoryError('ExperimentUserRepository', 'upsertExperimentUser', { user }, errorMsg);
+        throw errorMsgString;
+      });
+    return result.raw[0];
+  }
+
   public async saveRawJson(rawData: UserInput): Promise<ExperimentUser> {
     const result = await this.createQueryBuilder('experimentUser')
       .insert()
